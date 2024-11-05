f32d0d011c33ed28d6fff0f3ee00a34e4c7e2d80
BKOPYZ <breakoff1530@gmail.com>
2024-11-05 09:35:38 +0700

    refactor: database from mysql to sqlite3 and fix after sign up"

--- userauths/views.py
+++ userauths/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
 
 
 def register_view(request):
-    return render(request, "userauths/sign-up.html")
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            new_user = authenticate(
+                username=form.cleaned_data["email"],
+                password=form.cleaned_data["password1"],
+            )
+            login(request, new_user)
+            return redirect("core:index")
+    form = UserRegisterForm
+
+    context = {
+        "form": form,
+    }
+    return render(request, "userauths/sign-up.html", context)
