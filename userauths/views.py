49ee8a2a6e9c731ceb719aca13372343e56bf44b
BKOPYZ <breakoff1530@gmail.com>
2024-11-05 11:25:18 +0700

    comment: todo on view of userauths

--- userauths/views.py
+++ userauths/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, authenticate
 
 def register_view(request):
     if request.method == "POST":
+        # TODO: imeplement in a way that take the input from post
         form = UserRegisterForm(request.POST or None)
         if form.is_valid():
             form.save()
@@ -20,3 +21,8 @@ def register_view(request):
         "form": form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    # TODO: implement me
+    pass
