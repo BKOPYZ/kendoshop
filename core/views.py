d645810b4a4db8b33e2a4c50c66dd6002efc6e93
BKOPYZ <breakoff1530@gmail.com>
2024-11-11 01:44:57 +0700

    fix: too large file

--- core/views.py
+++ core/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from core.models import Product
 
 
 # Create your views here.
 def index(request):
     return render(request, "core/index.html")
 
+
 def home(request):
-    return render(request, 'core/home.html')
+    return render(request, "core/home.html")
+
 
 def product(request):
-    return render(request, 'core/product.html')
+    all_product = Product.objects.all()
+
+    content = {"products": all_product}
+    return render(request, "core/product.html", content)
