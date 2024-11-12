3e811d6bc637a5d0bafc22f0ca8ed5f58f2a16f2
BKOPYZ <breakoff1530@gmail.com>
2024-11-12 16:23:45 +0700

    feat: login view

--- userauths/views.py
+++ userauths/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
-from userauths.models import User
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 def register_view(request):
@@ -16,6 +18,10 @@ def register_view(request):
         password = post.get("password")
         confirm_password = post.get("confirm_password")
 
+        context["username"] = username
+        context["email"] = email
+        context["userprofile"] = user_profile
+
         # error_message = ""
         is_valid = True
 
@@ -28,34 +34,53 @@ def register_view(request):
             is_valid = False
 
         if is_valid:
-
-            new_user = User.objects.create(
-                user_profile=user_profile,
-                username=username,
-                email=email,
-                first_name=first_name,
-                last_name=last_name,
-            )
-            new_user.set_password(password)
-            new_user.save()
-            messages.info(request, "Thanks for registering. You are now logged in.")
-            new_user = authenticate(
-                username=email,
-                password=password,
-            )
-            print("new_user")
-            print(new_user)
-            login(request, new_user)
-            return redirect("core:index")
-
-        else:
-            context["username"] = username
-            context["email"] = email
-            context["userprofile"] = user_profile
-    print("not ok")
+            try:
+                new_user = User.objects.create(
+                    user_profile=user_profile,
+                    username=username,
+                    email=email,
+                    first_name=first_name,
+                    last_name=last_name,
+                )
+                new_user.set_password(password)
+                new_user.save()
+                messages.info(request, "Thanks for registering. You are now logged in.")
+                new_user = authenticate(
+                    username=email,
+                    password=password,
+                )
+                login(request, new_user)
+                return redirect("core:home")
+            except Exception as e:
+                messages.error(request, e)
     return render(request, "userauths/sign-up.html", context)
 
 
 def login_view(request):
-    # TODO: implement me
-    pass
+    if request.user.is_authenticated:
+        return redirect("core:home")
+
+    context = dict()
+    if request.method == "POST":
+        post = request.POST
+        email = post.get("email")
+        password = post.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExists:
+            messages.error(request, "email does not exist")
+        else:
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+
+                login(request, user)
+                messages.success(request, "you are login")
+                return redirect("core:home")
+            else:
+                messages.error(
+                    request, "user doesnot exist or password does not correct"
+                )
+
+    return render(request, "userauth/login.html", context)
