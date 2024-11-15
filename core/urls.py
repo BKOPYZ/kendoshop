5c6973cce9f32d5c10fd7fbe9aca400a0c2f6aae
BKOPYZ <breakoff1530@gmail.com>
2024-11-15 13:38:20 +0700

    feat: scratch pagination

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("shop/", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("product/", views.product, name="product"),
+    path("product/page/<int:page>", views.product, name="product"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from core.models import Product
+import math
+
+PRODUCTS_PER_PAGE = 3
 
 
 # Create your views here.
@@ -11,8 +14,26 @@ def home(request):
     return render(request, "core/home.html")
 
 
-def product(request):
-    all_product = Product.objects.all()
+def product(request, page: int | None = None):
+    all_products = Product.objects.all()
 
-    content = {"products": all_product}
+    num_products = len(all_products)
+
+    num_pages = math.ceil(num_products / PRODUCTS_PER_PAGE)
+
+    page = 1 if page is None else page
+
+    page_products = all_products[
+        (page - 1) * PRODUCTS_PER_PAGE : max(num_pages, PRODUCTS_PER_PAGE * page)
+    ]
+    before = range(max(1, page - 2), page)
+    after = range(page, min(page + 3, num_pages + 1))
+
+    content = {
+        "products": page_products,
+        "num_pages": num_pages,
+        "page": page,
+        "before": before,
+        "after": after,
+    }
     return render(request, "core/product.html", content)
