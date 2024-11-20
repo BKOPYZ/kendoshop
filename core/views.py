715e09f30dcb412f0fc037dee0d33024ee83b88d
BKOPYZ <breakoff1530@gmail.com>
2024-11-20 10:33:32 +0700

    fix: product bug

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from core.models import Product
 from django.contrib import messages
+from django.db.models import Count
 import math
 
 PRODUCTS_PER_PAGE = 3
@@ -19,12 +20,16 @@ def home_view(request):
 def product_view(request, category: str | None = None, page: int | None = None):
     if category is None:
 
-        categorize_product = Product.objects.all()
+        categorize_product = Product.objects.raw(
+            "Select * from core_product group by name"
+        )
         category = "all product"
 
     else:
-
-        categorize_product = Product.objects.filter(product_type=category)
+        categorize_product = Product.objects.raw(
+            f"Select * from core_product where product_type = '{category}' group by name"
+        )
+    print(categorize_product)
 
     num_products = len(categorize_product)
 
@@ -58,42 +63,43 @@ def product_detail_view(request, product_id: int, **kwargs):
         all_same_product = Product.objects.filter(name=product.name)
         context = dict()
 
-        if product.category == "armor":
+        if product.product_type == "armor":
 
             all_same_product = sorted(
                 all_same_product,
                 key=lambda product: size_order.index(product.armor_size),
             )
 
-        elif product.category == "uniform":
+        elif product.product_type == "uniform":
+            all_available_color = all_same_product.values("uniform_color").distinct()
             all_same_product = sorted(
                 all_same_product,
                 key=lambda product: size_order.index(product.uniform_size),
             )
-            all_available_color = all_same_product.distinct("uniform_color")
             available_color = Product.objects.filter(
-                name=product.name, uniform_size=all_same_product[0].uniform_size
-            ).distinct("uniform_color")
+                    name=product.name, uniform_size=all_same_product[0].uniform_size).values("uniform_color").distinct()
+            
             context["available_color"] = available_color
             context["all_available_color"] = all_available_color
 
-        elif product.category == "sword":
+        elif product.product_type == "sword":
             all_same_product = sorted(
-                all_same_product,
-                key=lambda product: size_order.index(product.sword_length),
+                all_same_product, key=lambda product: product.sword_length
             )
 
         product = all_same_product[0]
 
         context["product"] = product
+        context["all_same_product"] = all_same_product
         return render(request, "core/product_detail.html", context)
 
     except Product.DoesNotExist:
         messages.warning(request, "product does not exist")
         return redirect("core:product")
-    
+
+
 def select_same_product(request, product_id: int):
-        return redirect("core:product")
+    return redirect("core:product")
 
 
 def go_back(request):
